ee8da3bb1db8567bb4230f0c4845d011ea45bdc6
David Hacker <davidhackerdvm@gmail.com>
2012-05-03 23:14:10 -0500

    fix egl.cfg update wifi settings

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -36,8 +36,6 @@ BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 
 
 # Connectivity - Wi-Fi
-USES_TI_MAC80211 := true
-ifdef USES_TI_MAC80211
 BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
 WPA_SUPPLICANT_VERSION           := VER_0_8_X
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_wl12xx
@@ -45,11 +43,10 @@ BOARD_HOSTAPD_DRIVER             := NL80211
 BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_wl12xx
 BOARD_WLAN_DEVICE                := wl12xx_mac80211
 BOARD_SOFTAP_DEVICE              := wl12xx_mac80211
-WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/wl12xx.ko"
-WIFI_DRIVER_MODULE_NAME          := "wl12xx"
+WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/wl12xx_sdio.ko"
+WIFI_DRIVER_MODULE_NAME          := "wl12xx_sdio"
 WIFI_FIRMWARE_LOADER             := ""
 COMMON_GLOBAL_CFLAGS += -DUSES_TI_MAC80211
-endif
 
 # Audio
 BOARD_USES_GENERIC_AUDIO := false
